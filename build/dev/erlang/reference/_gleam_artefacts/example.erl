b4fac3ee31d0a75fedcb74c5bdeb08f13fba56ca
Eli Adelhult <hello@eli.nu>
2024-09-23 20:17:10 +0200

    fix gitignore

--- build/dev/erlang/reference/_gleam_artefacts/example.erl
+++
@@ -1,8 +0,0 @@
--module(example).
--compile([no_auto_import, nowarn_unused_vars, nowarn_unused_function, nowarn_nomatch]).
-
--export_type([reference_/0]).
-
--type reference_() :: {reference, list(binary()), binary(), binary()}.
-
-
--- build/dev/erlang/reference/include/example_Reference.hrl
+++
@@ -1,5 +0,0 @@
--record(reference, {
-    path :: list(binary()),
-    module_doc :: binary(),
-    content :: binary()
-}).
